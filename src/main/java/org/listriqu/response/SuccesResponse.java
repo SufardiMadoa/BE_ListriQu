4a57b938135e569f1f69f5e4922c656e0b6c386c
SufardiMadoa <Sufardimpv@gmail.com>
2025-06-18 08:10:41 +0700

    fix: api-role

---
+++ src/main/java/org/listriqu/response/SuccesResponse.java
@@ -0,0 +1,11 @@
+package org.listriqu.response;
+
+public class SuccesResponse {
+       public String status;
+    public String message;
+
+    public SuccesResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ src/main/java/org/listriqu/response/UnitResponse.java
@@ -0,0 +1,15 @@
+package org.listriqu.response;
+
+
+public class UnitResponse {
+    public Integer unitId;
+    public String unitCode;
+    public String unitName;
+    public String description;
+    public Integer parentUnitId;
+    public String address;
+    public String phone;
+    public String email;
+    public Integer managerId;
+    public String status;
+}
